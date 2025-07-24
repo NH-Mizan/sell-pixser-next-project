2df6b833b0def12f789c411e27c54a03aba5a988
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-07-25 02:03:32 +0600

    home page product do not show next

--- src/components/Home/homeproducts.jsx
+++ src/components/Home/homeproducts.jsx
@@ -1,10 +1,31 @@
+'use client';
+import { useEffect, useState } from "react";
 
 export default function HomeProducts() {
+    const [products, setProducts] = useState([]);
+    const baseURL = 'https://sellpixer.websolutionit.com/';
+      useEffect(() => {
+        const fetchproduct = async () => {
+          try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/hotdeal-product`);
+            const data = await res.json();
+            if (data?.data) {
+              setProducts(data.data.slice(0, 20));
+              console.log(data.data.slice(0, 20).map(product => product.id==7));
+            }
+          } catch (error) {
+            console.error('Slider fetch error:', error);
+          }
+        };
 
+    
+        fetchproduct();
+      }, []);
+   
 
     return (
         <div className="w-10/12 mx-auto py-8">
-            <h1 className="text-3xl font-bold text-gray-800 mb-8">Special Offers</h1>
+            <h1 className="text-3xl font-bold text-gray-800 mb-8">Home Products</h1>
 
            
         </div>
